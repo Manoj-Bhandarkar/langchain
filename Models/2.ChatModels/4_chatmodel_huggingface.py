f33d1f602f3d2db35d4643e4d3808f4904a5d1eb
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-09-11 08:46:44 +0530

    LangChain - Models - ChatModels - HuggingFace Inference API - Llama

--- Models/2.ChatModels/4_chatmodel_huggingface.py
+++ Models/2.ChatModels/4_chatmodel_huggingface.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
     task="text-generation"
 )
 
@@ -12,4 +12,4 @@ hf_model = ChatHuggingFace(llm=llm)
 
 result = hf_model.invoke("What is the capital of India")
 
-print(result)
+print(result.content)
